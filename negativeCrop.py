41c36361e605d6fd42b366062dfd25a15eed48f1
Anh Dinh <dinhaq1@uci.edu>
2018-05-28 15:20:09 -0700

    added negative cropping script

---
+++ negativeCrop.py
@@ -0,0 +1,55 @@
+
+
+
+import json
+import cv2, random
+import matplotlib.pyplot as plt
+import numpy as np
+
+def negCropping():
+
+    # open the json file
+    jsonData = json.load(open("./name_and_box.json","r"))
+    names = jsonData.keys()
+
+    # declare how many photo you wanna check out here
+    count = 1
+    for name in names:
+        if count == 3:
+            break
+        count +=1
+
+        # load in the image
+    	img = cv2.imread("./Color/" + name[:-2] + ".jpg")
+
+        # grab the center of the box of the hand to avoid
+        centerX = int(jsonData[name][0])
+        centerY = int(jsonData[name][1])
+        # now just need to start sliding the window little by litte
+        # maybe do a for loop here !!
+        dimension = img.shape
+        endCol = dimension[1] # the last column
+        endRow = dimension[0] # the last row
+
+        for row in xrange(0,endCol-170,170):
+            # i think here we need to add a check to see whether or not we have
+            # fallen out of bound in row
+            if row + 170 > endRow:
+                break
+            for col in xrange(0,endRow-170,170):
+                # need to add a check that even if the col is wihin the range of the
+                # box that contains the hand, don't crop
+                #
+                crop_img = img[ row :row+170  , col : col+170] # crop a window of 170x170 pixels
+            	plt.imshow(crop_img[:,:,::-1])
+            	plt.pause(0.05) # pause for 3 secs
+
+                # save the image in a folder
+                # need to decide how to name the negative patches as well!
+                # cv2.imwrite("neg_processed_data/" + str(name)+".jpg", crop_img)
+
+
+
+# call the method we wrote to plot the box!
+negCropping();
+print "Done!"
