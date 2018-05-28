e928034684d2fabd8e14edc70310dad995263131
Anh Dinh <dinhaq1@uci.edu>
2018-05-28 16:02:31 -0700

    fix some stuff in negCrop.py

--- negativeCrop.py
+++ negativeCrop.py
@@ -15,7 +15,7 @@ def negCropping():
     # declare how many photo you wanna check out here
     count = 1
     for name in names:
-        if count == 3:
+        if count == 2:
             break
         count +=1
 
@@ -31,24 +31,23 @@ def negCropping():
         endCol = dimension[1] # the last column
         endRow = dimension[0] # the last row
 
-        for row in xrange(0,endCol-170,170):
-            # i think here we need to add a check to see whether or not we have
-            # fallen out of bound in row
-            if row + 170 > endRow:
-                break
-            for col in xrange(0,endRow-170,170):
+        countImages =0
+
+        for row in xrange(0,endRow-170,170):
+            for col in xrange(0,endCol-170,170):
                 # need to add a check that even if the col is wihin the range of the
                 # box that contains the hand, don't crop
                 #
                 crop_img = img[ row :row+170  , col : col+170] # crop a window of 170x170 pixels
             	plt.imshow(crop_img[:,:,::-1])
             	plt.pause(0.05) # pause for 3 secs
+                countImages +=1
 
                 # save the image in a folder
                 # need to decide how to name the negative patches as well!
-                # cv2.imwrite("neg_processed_data/" + str(name)+".jpg", crop_img)
+                cv2.imwrite("neg_processed_data/" + str(name[:-2]) +"_"+ str(countImages)+".jpg", crop_img)
 
-
+        print countImages
 
 # call the method we wrote to plot the box!
 negCropping();
